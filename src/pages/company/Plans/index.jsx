dbfd69aa6729b63c104a77235ac2f7ac792714a0
Amirxon <amirxon@MacBook-Air-alisher.local>
2025-06-11 16:31:28 +0500

    reload

--- src/pages/company/Plans/index.jsx
+++ src/pages/company/Plans/index.jsx
@@ -3,7 +3,8 @@ import './style.css'
 export const Plans = () => {
   return (
     <div>
-      
+      <h3>Subscription Plan Management</h3>
+      <p>Manage your subscription plan and billing details to optimize your recruiting process.</p>
     </div>
   )
 }
