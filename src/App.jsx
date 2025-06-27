e4088efd25bb6b10bef498ad5ad8634996ede886
abduvoris0408 <abduvorismominov20408@gmail.com>
2025-06-27 14:08:52 +0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,64 @@ import { Route, Routes } from 'react-router-dom'
 
 import ForgotPassword from './components/auth/Login/ForgotPassword'
 import LoginPage from './components/auth/Login/LoginPage'
+import Verify from './components/auth/Signup/authVerify/Verify'
 import SignupPage from './components/auth/Signup/SignupPage'
+import LandingPage from './pages/LandingPage'
+
+import CandidateCompleteProfile from './components/auth/Signup/complete/CandidateCompleteProfile'
+import CompanyCompleteProfile from './components/auth/Signup/complete/CompanyCompleteProfile'
+import RecruiterCompleteProfile from './components/auth/Signup/complete/RecruiterCompleteProfile'
+
+import { Feedback, JoinInterviews, ProfileCandidate } from './pages/candidate'
+import { Plans } from './pages/company/Plans'
+import { ProfileCompany } from './pages/company/ProfileCompany'
+import { InterviewsRec, ProfilerecruiterRec } from './pages/recruiter'
+
 import { AdminDashboard } from './components/dashboards/AdminDashboard/AdminD'
 import { CandidateDashboard } from './components/dashboards/CandidateDashboard/CandidateD'
 import { CompanyDashboard } from './components/dashboards/CompanyDashboard/CompanyD'
 import { RecruiterDashboard } from './components/dashboards/RecruiterDashboard/RecruiterD'
-import { Feedback, JoinInterviews, ProfileCandidate } from './pages/candidate'
-import { Plans } from './pages/company/Plans'
-import { ProfileCompany } from './pages/company/ProfileCompany'
-import LandingPage from './pages/LandingPage'
-import { ProfilerecruiterRec } from './pages/recruiter'
-import { InterviewsRec } from './pages/recruiter/InterviewsRec'
+
+import {
+	AdminsTable,
+	CandidateTable,
+	InterviewsTable,
+	PaymentsTable,
+	RecruiterTable,
+	ReportsTable,
+} from './pages/admin'
+import { ComapaniesTable } from './pages/admin/companies-table'
+import DashboardCompany from './pages/company/DashboardCompany'
+
+import CandidateHome from './pages/candidate/Dashboard'
+
 function App() {
 	return (
 		<Routes>
+			{/* Public */}
 			<Route path='/' element={<LandingPage />} />
 			<Route path='/login' element={<LoginPage />} />
-			<Route path='/forgot-password' element={<ForgotPassword />} />
 			<Route path='/signup' element={<SignupPage />} />
+			<Route path='/forgot-password' element={<ForgotPassword />} />
+			<Route path='/auth/verify' element={<Verify />} />
 
-			{/* Dashboards */}
+			{/* Complete profile routes */}
+			<Route
+				path='/complete-profile/candidate'
+				element={<CandidateCompleteProfile />}
+			/>
+			<Route
+				path='/complete-profile/recruiter'
+				element={<RecruiterCompleteProfile />}
+			/>
+			<Route
+				path='/complete-profile/company'
+				element={<CompanyCompleteProfile />}
+			/>
 
-			{/* Candidate */}
+			{/* Candidate Dashboard */}
 			<Route path='/candidate-dashboard' element={<CandidateDashboard />}>
+				<Route index element={<CandidateHome />} />
 				<Route path='feedback' element={<Feedback />} />
 				<Route path='join-interview' element={<JoinInterviews />} />
 				<Route
@@ -33,17 +68,26 @@ function App() {
 				/>
 			</Route>
 
-			{/* Admin */}
-			<Route path='/admin-dashboard' element={<AdminDashboard />}></Route>
+			{/* Admin Dashboard */}
+			<Route path='/admin-dashboard' element={<AdminDashboard />}>
+				<Route index element={<ComapaniesTable />} />
+				<Route path='candidate-table' element={<CandidateTable />} />
+				<Route path='interviews-table' element={<InterviewsTable />} />
+				<Route path='payments-table' element={<PaymentsTable />} />
+				<Route path='recruiters-table' element={<RecruiterTable />} />
+				<Route path='reports-table' element={<ReportsTable />} />
+				<Route path='admins-table' element={<AdminsTable />} />
+			</Route>
 
-			{/* Company */}
+			{/* Company Dashboard */}
 			<Route path='/company-dashboard' element={<CompanyDashboard />}>
+				<Route index element={<DashboardCompany />} />
 				<Route path='plans' element={<Plans />} />
 				<Route path='profile-company' element={<ProfileCompany />} />
 			</Route>
 
-			{/* Recruiter */}
-			<Route path='/recruiter-dashboard' element={<RecruiterDashboard />}>
+			{/* Recruiter Dashboard */}
+			<Route path='/recruiter' element={<RecruiterDashboard />}>
 				<Route path='interviews-rec' element={<InterviewsRec />} />
 				<Route
 					path='profile-recruiter'
